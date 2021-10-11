1182bef0d02e9b6acc977371333be73606c835a0
ulasdev <ulascansenturk@protonmail.com>
2021-10-12 02:02:32 +0300

    IBasketRepository and the BasketRepository created and coded...

---
+++ src/Services/Basket/Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace Basket.Api.Repositories
+{
+    public class BasketRepository:IBasketRepository
+    {
+        
+        private readonly IDistributedCache _redisCache;
+        
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+            
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+
+            if (String.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+          await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.Api/Repositories/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Basket.Api.Entities;
+
+namespace Basket.Api.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        
+        Task<ShoppingCart> UpdateBasket (ShoppingCart basket);
+        
+        Task DeleteBasket(string userName);
+    }
+}
